ede81807c2ee2a7305f338f34b8dc574ed298af4
cscranton5 <carterscranton5@gmail.com>
2025-04-22 12:14:56 -0400

    MongoDB setup / API endpoints up and running

--- server/setup.sh
+++ server/setup.sh
@@ -1,22 +1,27 @@
 
+# setup django virtual enviroment
 pip install virtualenv
 virtualenv djangoenv
 source djangoenv/bin/activate
 
+# install requirements + do django migrations
 python3 -m pip install -U -r requirements.txt
-
 python3 manage.py makemigrations
-
 python3 manage.py migrate
+#python3 manage.py runserver
 
+# git config setup
 git config --global user.name cscranton5
 git config --global user.email carterscranton5@gmail.com
 
+# front end setup
 cd ./frontend
-
 npm install
-
 npm run build
 
 cd ..
-#python3 manage.py runserver
+
+# database server setup
+cd ./database
+docker build . -t nodeapp
+#docker-compose up
