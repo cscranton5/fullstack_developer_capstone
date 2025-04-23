070bc4a4ca901dcc6fe29a46891b1d9bbcb71c36
cscranton5 <carterscranton5@gmail.com>
2025-04-23 19:47:57 -0400

    sentiment analyser fix

--- server/setup.sh
+++ server/setup.sh
@@ -1,14 +1,10 @@
-
 # setup django virtual enviroment
 pip install virtualenv
 virtualenv djangoenv
 source djangoenv/bin/activate
 
 # install requirements + do django migrations
-pip install virtualenv
-virtualenv djangoenv
-source djangoenv/bin/activate
-
+python3 -m pip install -U -r requirements.txt
 python3 manage.py makemigrations
 python3 manage.py migrate
 #python3 manage.py runserver
